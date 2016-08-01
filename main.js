26b36843acce2e179769202b31c613ea23404cb4
Mery Santos <rymesz@gmail.com>
2016-08-01 16:52:51 -0500

    prueba inicial

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 // Datos Iniciales
+var nro_cuenta;
 // Operaciones
-	
+
 // Retiro
+	function retiro(monto){
+	// Solo si presiona en un numero fijado
+		
+	}
+
+	function retiroEspecifico(){
+		var monto =$('');
+	}
+
+
 // Transferencia
 // Deposito
